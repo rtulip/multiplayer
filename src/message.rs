b6aa2c70377a0645458ad227ac0a78ede1dbacff
Robbie <robert.tulip97@gmail.com>
2019-10-31 12:54:22 -0700

    Created Message Trait

--- src/message.rs
+++ src/message.rs
@@ -2,10 +2,37 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use serde_json::json;
 
 use crate::threading::dispatcher;
 
 pub const MSG_SIZE: usize = 4096;
+pub const TEXT_MESSAGE_IDENTIFIER: &str = "Text";
+
+trait Message<'a>: Serialize + Deserialize<'a> {
+    const MSG_TYPE: &'a str;
+
+    fn to_json_string(&self) -> String {
+
+        let v = json!({
+            "msg_type": Self::MSG_TYPE.to_owned(),
+            "data": self,
+        });
+
+        v.to_string()
+    }
+
+    fn from_json_string(json_string: &'a str) -> serde_json::Result<Self> {
+
+        let v_res:serde_json::Result<Self> = serde_json::from_str(json_string);
+        match v_res {
+            Ok(msg) => Ok(msg),
+            Err(e) => Err(e),
+        }
+    
+    }
+
+}
 
 #[derive(Deserialize, Serialize)]
 pub struct TextMessage {
@@ -18,7 +45,7 @@ impl TextMessage{
     pub fn new<S: Into<String>>(text: S) -> TextMessage{
 
         TextMessage{
-            msg_type: "Text".to_owned(),
+            msg_type: TEXT_MESSAGE_IDENTIFIER.to_owned(),
             text: text.into(),
         }
 
@@ -27,6 +54,15 @@ impl TextMessage{
     pub fn handle(&self) {
         println!("Received Text Message: {}", self.text);
     }
+
+}
+
+pub struct RequestClientID {
+    msg_type: String,
+}
+
+pub struct RequestClientIDResponse{
+    msg_type: String,
 
 }
 
@@ -51,7 +87,7 @@ pub fn receive_json(buff: &Vec<u8>, dispatch: dispatcher::Dispatcher) {
     let string = String::from_utf8(msg).expect("Invlaid utf8 message");
 
     let v: Value = serde_json::from_str(string.as_str()).expect("Unable to convert to json");
-
+    println!("Json: {}", v);
     match &v["msg_type"] {
         Value::String(text) => {
             match text.as_ref() {
